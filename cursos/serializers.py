1ae7e6f8e7b407f6f955a2f9a18a051058f1a37c
Erick <dverickfernan@gmail.com>
2023-03-20 21:28:02 -0300

    Customizando a serialização dos dados

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.db.models import Avg
 
 from .models import Curso, Avaliacao
 
@@ -24,7 +25,7 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
     def validate_avaliacao(self, valor):  # A escrita tem que ser: validate_'campo_que_será_validado'
         if valor in range(1, 6):  # 1 2 3 4 5
             return valor
-        raise serializers.ValidationError('A avaliação precisa ser entre 1 e 5')
+        raise serializers.ValidationError('A avaliação precisa ser um inteiro entre 1 e 5')
 
 
 class CursoSerializer(serializers.ModelSerializer):
@@ -45,6 +46,8 @@ class CursoSerializer(serializers.ModelSerializer):
     # Primary Key Related Field
     avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
+    media_avaliacoes = serializers.SerializerMethodField()
+
     class Meta:
         model = Curso
         fields = (
@@ -53,5 +56,13 @@ class CursoSerializer(serializers.ModelSerializer):
             'url',
             'criacao',
             'ativo',
-            'avaliacoes'
+            'avaliacoes',
+            'media_avaliacoes'
         )
+
+    def get_media_avaliacoes(self, obj):  # obj é o objeto que está executando a fç
+        media = obj.avaliacoes.aggregate(Avg('avaliacao')).get('avaliacao__avg')
+
+        if media is None:
+            return 0
+        return round(media * 2) / 2
