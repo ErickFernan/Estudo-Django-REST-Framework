4bd1182d2857c9d279bcde57a39cc06e650f03fa
Erick <dverickfernan@gmail.com>
2023-03-21 08:48:14 -0300

    Instalando e utilizando o módulo JSONPATH

---
+++ teste_jsonpath.py
@@ -0,0 +1,24 @@
+import requests
+import jsonpath
+
+
+avaliacoes = requests.get('http://localhost:8000/api/v2/avaliacoes/')
+
+# resultados1 = avaliacoes.json()['results']
+# resultados = jsonpath.jsonpath(avaliacoes.json(), 'results')
+
+# print(resultados1)
+# print(resultados)
+
+# primeira = jsonpath.jsonpath(avaliacoes.json(), 'results[0]')
+# print(primeira)
+
+# nome = jsonpath.jsonpath(avaliacoes.json(), 'results[0].nome')
+# print(nome)
+
+# nota_dada = jsonpath.jsonpath(avaliacoes.json(), 'results[0].avaliacao')
+# print(nota_dada)
+
+# Todos os nomes das pessoas que avaliaram o curso
+nomes = jsonpath.jsonpath(avaliacoes.json(), 'results[*].nome')
+print(nomes)
